d602652d1e3130274b5bffe68b110eb40fbf12b6
Tahir Montgomery <montgomeryt@mail.usf.edu>
2021-04-03 15:47:22 -0400

    added changes

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,70 +1,81 @@
 <template>
   <mdb-container fluid>
-    <mdb-row class="full-height">
-      <mdb-col class="left">
-        <div class="register">
-          <h2 class="font-weight-bold">Register</h2>
-          <p class="lead">Please register your account</p>
-          <div class="form">
-            <!-- Email Input -->
+    <form class="needs-validation" novalidate @submit="register">
+      <mdb-row class="full-height">
+        <mdb-col class="left">
+          <div class="register">
+            <h2 class="font-weight-bold">Register</h2>
+            <p class="lead">Please register your account</p>
+            <div class="form">
+              <!-- Email Input -->
 
-            <mdb-input
-              required
-              id="first"
-              type="text"
-              label="Enter your first name"
-              outline
-              v-model="user.firstName"
-            />
-            <mdb-input
-              required
-              id="last"
-              type="text"
-              label="Enter your last name"
-              outline
-              v-model="user.lastName"
-            />
-            <mdb-input
-              required
-              id="email"
-              type="email"
-              label="Enter email"
-              outline
-              v-model="user.email"
-            />
+              <mdb-input
+                required
+                id="first"
+                type="text"
+                label="Enter your first name"
+                outline
+                v-model="user.firstName"
+              />
+              <mdb-input
+                required
+                id="last"
+                type="text"
+                label="Enter your last name"
+                outline
+                v-model="user.lastName"
+              />
+              <mdb-input
+                required
+                id="email"
+                type="email"
+                label="Enter email"
+                outline
+                v-model="user.email"
+              />
 
-            <!-- Password Input -->
-            <mdb-input
-              id="password"
-              type="password"
-              label="Enter password"
-              required
-              outline
-              v-model="user.password"
-            />
+              <select required id="email" class="browser-default custom-select">
+                <option value="null">Select role</option>
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+              </select>
 
-            <mdb-input
-              id="confirm"
-              type="password"
-              label="Confirm password"
-              required
-              outline
-              v-model="user.confirm"
-            />
+              <!-- Password Input -->
+              <mdb-input
+                id="password"
+                type="password"
+                label="Enter password"
+                required
+                outline
+                v-model="user.password"
+              />
 
-            <!-- Submit Button -->
-            <!-- Forgot Password -->
-            <div class="forgot_and_register">
-              <mdb-btn @click="register" id="login_btn" class="login_btn mr-3"
-                >REGISTER</mdb-btn
-              >
-              <router-link class="link" to="/login"
-                >Already have an account? Login here.</router-link
-              >
-            </div>
+              <mdb-input
+                id="confirm"
+                type="password"
+                label="Confirm password"
+                required
+                outline
+                v-model="user.confirm"
+              />
 
-            <!-- Third Party Sign in -->
-            <!-- <div class="third_party">
+              <!-- Submit Button -->
+              <!-- Forgot Password -->
+              <div class="forgot_and_register">
+                <mdb-btn
+                  type="submit"
+                  @click="register"
+                  id="login_btn"
+                  class="login_btn mr-3"
+                  >REGISTER</mdb-btn
+                >
+                <router-link class="link" to="/login"
+                  >Already have an account? Login here.</router-link
+                >
+              </div>
+
+              <!-- Third Party Sign in -->
+              <!-- <div class="third_party">
               <p>or</p>
               <div class="third_party_icons">
                 <mdb-icon
@@ -81,14 +92,15 @@
                 />
               </div>
             </div> -->
+            </div>
           </div>
-        </div>
-      </mdb-col>
-      <mdb-col class="right">
-        <h2 class="font-weight-bold font-italic">Database Design</h2>
-        <img class="img-fluid img" src="../assets/wallet1.svg" alt="Wallet" />
-      </mdb-col>
-    </mdb-row>
+        </mdb-col>
+        <mdb-col class="right">
+          <h2 class="font-weight-bold font-italic">Database Design</h2>
+          <img class="img-fluid img" src="../assets/wallet1.svg" alt="Wallet" />
+        </mdb-col>
+      </mdb-row>
+    </form>
   </mdb-container>
 </template>
 
@@ -109,7 +121,9 @@ export default {
     };
   },
   methods: {
-    async register() {
+    async register(event) {
+      event.preventDefault();
+      event.target.classList.add("was-validated");
       if (
         this.user.email == "" ||
         this.user.password == "" ||
